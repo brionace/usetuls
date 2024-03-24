31e3c7356f53a2d1e41ebe6d866b1f6297754e32
Brian Oryem <brian.oryem@brianory.me>
2024-03-24 21:51:39 +0000

    Add Google Analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import UIProvider from "./ui-provider";
+import GoogleAnalytics from "@/components/googleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
         <UIProvider>{children}</UIProvider>
       </body>
     </html>
