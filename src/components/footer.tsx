9a8e9eb750c5a23f87b6d8793f2b246c51a226de
Brian Oryem <brian.oryem@brianory.me>
2024-03-24 23:34:34 +0000

    Update footer component with additional links and styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,11 +3,18 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="py-4 text-center text-sm text-slate-500">
+    <footer className="flex gap-2 py-6 mt-8 px-6 justify-center text-sm text-slate-500 border-t border-slate-100">
       <p>
-        Made with ❤️ by{" "}
+        <span className="hidden md:inline">Made with</span> ❤️{" "}
+        <span className="hidden md:inline">by</span>{" "}
         <Link href="https://brianory.me" target="_blank">
           Brian
+        </Link>
+      </p>
+      <span>/</span>
+      <p className="bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+        <Link href="https://www.buymeacoffee.com/brianoryem" target="_blank">
+          Buy me a coffee
         </Link>
       </p>
     </footer>
